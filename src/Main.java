c5c1f791bcf77d681ac4225b12ed178396ccc6ca
DevSimtrue <simtrue@naver.com>
2022-11-29 10:11:44 +0900

    1. 기본 빌드패턴

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        NutritionFacts cocacola = new NutritionFacts.Builder(240, 8).calories(320).build();
+        System.out.println("===============================================");
+        System.out.println(cocacola);
+        System.out.println("===============================================");
+
     }
 }
